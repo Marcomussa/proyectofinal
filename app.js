14bbb6fec6b7e04ca19bbf4809e192d90310bf93
marcomussa <--global>
2021-09-19 22:25:01 -0300

    se agrego funcionalidad en el form log in implementado Javascript

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-const express = require ('express')
-const path = require ('path')
-const app = express ()
+// const express = require ('express')
+// const path = require ('path')
+// const app = express ()
 
-/*Config public */
-app.use(express.static(path.resolve(__dirname, 'public')))
-
-
-/*Routes*/
-
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'views/index.html' ))})
-
-app.get('/login', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'views/login.html' ))})
-
-app.get('/cart', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'views/productCart.html' ))})
-
-app.get('/product', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'views/productDetail.html' ))})
-
-app.get('/register', (req, res)=>{
-     res.sendFile(path.resolve(__dirname,'views/register.html' ))})
+// /*Config public */
+// app.use(express.static(path.resolve(__dirname, 'public')))
 
 
-/*Server*/
+// /*Routes*/
 
-app.listen (3000, () => console.log('Server On port 3000') )
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname,'views/index.html' ))})
+
+// app.get('/login', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname,'views/login.html' ))})
+
+// app.get('/cart', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname,'views/productCart.html' ))})
+
+// app.get('/product', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname,'views/productDetail.html' ))})
+
+// app.get('/register', (req, res)=>{
+//      res.sendFile(path.resolve(__dirname,'views/register.html' ))})
+
+
+// /*Server*/
+
+// app.listen (3000, () => console.log('Server On port 3000') )
+
+const btnCrearCuenta = document.querySelector('#btnCrearCuenta')
+const contenedorCrearCuenta = document.querySelector('#crearCuenta')
+function mostrarCrearCuenta(){
+    btnCrearCuenta.addEventListener('click', function(){
+        contenedorCrearCuenta.style.display = 'block'
+    })
+}
+
+mostrarCrearCuenta()
