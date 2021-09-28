4ed168997e757956a8d32ceb325d8d9a2263b8ff
GezaM <gezaejm@gmail.com>
2021-09-28 15:12:32 -0300

    seteos de express server

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
 const express = require ('express')
-const path = require ('path')
 const app = express ()
 
 /*Config public */
-app.use(express.static(path.resolve(__dirname, 'public')))
+app.use(express.static('public'));
 
 
 /*Routes*/
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'views/index.html'))
-})
+    res.sendFile(__dirname +'/views/index.html');})
 
-app.get('/login', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'views/index.html'))
-})
 
-app.get('/cart', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'views/productCart.html'))
-})
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');})
+
+
+app.get('/cart', (req, res)=>{ 
+    res.sendFile(__dirname +'/views/productCart.html');})
+
 
 app.get('/product', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'views/productDetail.html'))
-})
+    res.sendFile(__dirname + '/views/productDetail.html');})
 
 app.get('/wishlist', (req, res)=>{
-     res.sendFile(path.resolve(__dirname,'views/wishlist.html'))
-    })
+     res.sendFile(__dirname + '/views/wishlist.html');})
 
 
 /*Server*/
