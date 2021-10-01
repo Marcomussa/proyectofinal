a734a33046d11f22551979f31ddbcb1e99aa8cea
Marcomussa <marcomussa567@gmail.com>
2021-10-01 20:07:51 -0300

    se agrego codigo de express y funcionalidad pero hay un problema con las rutas por lo que el codigo a implementar quedo comentado

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 const express = require ('express')
 const app = express ()
+const productController = require('./controllers/productController')
 
 /*Config public */
 app.use(express.static('public'));
 
-
 /*Routes*/
+app.get('/', (req, res) => {
+    res.sendFile(__dirname +'/views/index.html')
+})
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname +'/views/index.html');})
+// app.get('/', productController.index)
+
+app.get('/login', (req,res) => {
+    res.sendFile(__dirname + '/views/login.html')
+})
+
+// app.get('/login', productController.login)
+
+app.get('/cart', (req, res) => { 
+    res.sendFile(__dirname +'/views/productCart.html')
+})
+
+// app.get('/cart', productController.cart)
+
+app.get('/product', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetail.html')
+})
+
+// app.get('/product', productController.product)
 
 
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');})
+app.get('/wishlist', (req, res) => {
+     res.sendFile(__dirname + '/views/wishlist.html')
+})
 
-
-app.get('/cart', (req, res)=>{ 
-    res.sendFile(__dirname +'/views/productCart.html');})
-
-
-app.get('/product', (req, res)=>{
-    res.sendFile(__dirname + '/views/productDetail.html');})
-
-app.get('/wishlist', (req, res)=>{
-     res.sendFile(__dirname + '/views/wishlist.html');})
-
+// app.get('/wishlist', productController.wishlist)
 
 /*Server*/
-
 app.listen (3030, () => console.log('Server On port 3030') )
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,21 @@
 const controlador = {
     index: function(req, res){
-        res.send('Index')
+        res.sendFile(__dirname +'/views/index.html')
+    },
+    login: function(req, res){
+        res.sendFile(__dirname + '/views/login.html')
+    },
+    cart: function(req, res){
+        res.sendFile(__dirname +'/views/productCart.html')
+    },
+    product: function(req, res){
+        res.sendFile(__dirname + '/views/productDetail.html')
+
+    },
+    wishlist: function(req, res){
+        res.sendFile(__dirname + '/views/wishlist.html')
     }
 }
+
 
 module.exports = controlador
