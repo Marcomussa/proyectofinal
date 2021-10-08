fd882eedabb05d5207cd38f4e024768ae460d83c
Marcomussa <marcomussa567@gmail.com>
2021-10-08 15:44:33 -0300

    se agreo ejs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require ('express')
 const app = express()
 const router = express.Router()
 const productController = require('./src/controllers/productController')
+const ejs = require('ejs')
+const path = require('path')
 
 /*Config public */
 app.use(express.static('public'));
