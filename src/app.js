84e3f5b48ba46f417f86b3d6d36b5c1fd2ea8b0b
Marcomussa <marcomussa567@gmail.com>
2021-10-11 22:23:09 -0300

    se agrego funcionalidad

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const mainRoutes = require('./routes/main')
+const productsRoutes = require('./routes/products')
+const usersRoutes = require('./routes/users')
+const path = require('path')
+
+// Accediendo a recursos estaticos: 
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.set('views', path.resolve(__dirname, 'views'))
+// app.use(express.static('src'))
+
+// Configurar EJS: 
+app.set('view engine', 'ejs')
+
+// Rutas:
+app.get('/', mainRoutes)
+
+app.get('/users', usersRoutes)
+
+app.get('/products', productsRoutes)
+
+
+// Server:
+app.listen (3000, () => console.log('Server On http://localhost:3000') )
