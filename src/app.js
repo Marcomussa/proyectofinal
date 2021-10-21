38c198ccfdda187898a4e3704de8cd7fd180f5c9
Marcomussa <marcomussa567@gmail.com>
2021-10-21 20:49:27 -0300

    se agrego logica, ahora los productos vienen desde un JSON en una db

--- src/app.js
+++ src/app.js
@@ -14,8 +14,6 @@ app.set('views', path.resolve(__dirname, 'views'))
 // Configurar EJS: 
 app.set('view engine', 'ejs')
 
-
-
 // Rutas:
 app.use('/', mainRoutes)
 
@@ -27,6 +25,6 @@ app.use('/products', productsRoutes)
 
 // Server:
 app.listen (3000, () => console.log('Server On http://localhost:3000') )
-app.use((req, res, next) => {
-    res.status(404).render('not-found');
-});
+// app.use((req, res, next) => {
+//     res.status(404).render('not-found');
+// });
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,3 +1,8 @@
+const path = require('path')
+const fs = require('fs')
+const productsFilePath = path.join(__dirname, '../../db/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 let productController = {
     cart: function(req, res){
         res.render('productCart') 
@@ -12,7 +17,24 @@ let productController = {
         res.render('modProduct')
     },
     list: function(req, res){
-        res.render('productList')
+        let productList = {
+            id: [],
+            name: [],
+            description: [],
+            price: [],
+            discount: [],
+            category: [],
+            products: products
+        }
+        products.forEach(par => {
+            productList.id.push(par.id)
+            productList.name.push(par.name)
+            productList.description.push(par.description)
+            productList.price.push(par.price)
+            productList.discount.push(par.discount)
+            productList.category.push(par.category)
+        })
+        res.render('productList', {'productsList': productList})
     }
 }
 
