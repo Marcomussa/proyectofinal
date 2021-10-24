4afca9f2341ee20801a4e3881a67220d853a5477
Marcomussa <marcomussa567@gmail.com>
2021-10-24 17:28:59 -0300

    mas funcionalidad multer, hay errores todavia no llegan los datos de la img pero se corrije

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,31 +4,32 @@ const productController = require('../controllers/productController')
 const multer = require('multer')
 const path = require('path')
 
-router.get('/', productController.list)
-
-router.get('/cart', productController.cart)
-
-router.get('/create', productController.create)
-    
-router.get('/mod', productController.mod)
-
-router.post('/redirect', productController.redirect)
-
+// Configuracion Multer:
 let multerDiskStorage = multer.diskStorage({
     destination: function(req, file, cb){
         let folder = path.join(__dirname,'../../public/img/groups')
         cb(null, folder)
     },
     filename: function(req, file, cb){
+        console.log(file)
         let imageName = Date.now() + path.extname(file.originalname)
         cb(null, imageName)
     }
 })
-
 let fileUpload = multer({'storage': multerDiskStorage})
 
-router.post('/', fileUpload.single('imagenUsuario'))
-productController.redirect
+// Rutas:
+router.get('/', productController.list)
+
+router.get('/cart', productController.cart)
+
+router.get('/create', productController.create)
+
+router.post('/', fileUpload.single('imagenProducto'), productController.redirect)
+    
+router.get('/mod', productController.mod)
+
+router.post('/redirect', productController.redirect)
 
 router.get('/:id', productController.productDetail)
 
