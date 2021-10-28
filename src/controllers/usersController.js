8be38c1c4192577bf083de95c8b3ed608163b66b
Marcomussa <marcomussa567@gmail.com>
2021-10-28 19:09:56 -0300

    a

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,13 @@
+const {validationResult} = require('express-validator')
+
 const usersController = {
     login: function(req, res){
-        res.render('login')
+        let errors = validationResult(req)
+        const validaciones = errors.array()
+        console.log(validaciones)
+        res.render('login', {
+            validaciones: validaciones
+        })
     },
     wishlist: function(req, res){
         res.render('wishlist')
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
+const {check} = require('express-validator')
 
 router.get('/', usersController.login)
+
+router.post('/', [
+    check('name').isEmpty(),
+    check('surname').isEmpty(),
+    check('email').isEmpty().isEmail(),
+    check('pass').isEmpty()
+], usersController.login)
+
+router.get('/redirect', function(req, res){
+    res.send(req.body)
+})
 
 router.get('/wishlist', usersController.wishlist)
 
