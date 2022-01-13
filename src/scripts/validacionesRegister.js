33ca53d902ba41f4dfb058c1821b77686ee4dc24
Marcomussa <marcomussa567@gmail.com>
2022-01-13 19:21:02 -0300

    mas validaciones

--- src/scripts/validacionesRegister.js
+++ src/scripts/validacionesRegister.js
@@ -1,33 +1,53 @@
+
 let qs = (par) => {
     return document.querySelector(par)
-}
-
-// Llamados a los inputs
-let username = qs('.nameRegister')
-let surname = qs('.surnameRegister')
-let email = qs('.emailRegister')
-let password = qs('.passwordRegister')
-let submit = qs('#btnCrearCuenta')
-
-// Llamados a los mensajes de inputs
-let usernameMsg = qs('.userNameRegisterlMsg')
-let surnameMsg = qs('.passRegisterMsg')
-let emailMsg = qs('.emailRegisterMsg')
-let passwordMsg = qs('.passwordRegisterMsg')
-
-function validacionesRegister(e) {
+  }
+  
+  // Llamados a los inputs
+  let username = qs('.nameRegister')
+  let surname = qs('.surnameRegister')
+  let email = qs('.emailRegister')
+  let password = qs('.passwordRegister')
+  let submit = qs('#btnCrearCuenta')
+  
+  // Llamados a los mensajes de inputs
+  let usernameMsg = qs('.usernameRegisterlMsg')
+  let surnameMsg = qs('.surnameRegisterMsg')
+  let emailMsg = qs('.emailRegisterMsg')
+  let passwordMsg = qs('.passwordRegisterMsg')
+  
+  function validacionesRegister(e) {
     if(username.value === ''){
         e.preventDefault()
+        usernameMsg.className = 'displayBlock'
     }
     if(surname.value === ''){
         e.preventDefault()
+        surnameMsg.className = 'displayBlock'
     }
     if(password.value === ''){
         e.preventDefault()
+        passwordMsg.className = 'displayBlock'
     }
     if(email.value === ''){
         e.preventDefault()
+        emailMsg.className = 'displayBlock'
     }
-}   
-
-submit.addEventListener('click', validacionesRegister)
+  
+    if(username.value != ''){
+        usernameMsg.className = 'displayNone'  
+    }
+    if(surname.value != ''){
+        surnameMsg.className = 'displayNone'
+    }
+    if(email.value != ''){
+        emailMsg.className = 'displayNone'  
+    }
+    if(password.value != ''){
+        passwordMsg.className = 'displayNone'
+    }
+  }   
+  
+  submit.addEventListener('click', validacionesRegister)
+  
+  
