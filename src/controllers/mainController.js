60039d695c695d1c6f31ec69b185c5f607925f22
Marcomussa <marcomussa567@gmail.com>
2022-01-25 20:56:36 -0300

    mas funcionalidad

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -73,16 +73,74 @@ let mainController = {
         .catch(err => console.log(err))
     },
     apiProdsInCategory: (req, res) => {
+        let count_1 = 0
+        let count_2 = 0
+        let count_3 = 0
+        let count_4 = 0
+        let count_5 = 0
+        let count_6 = 0
+        let count_7 = 0
+
         Products.findAll()
         .then((par) => {
-            par.forEach((data) => {
-                if(data.category_id == 3) {
-                    console.log(data.length)
+            for(let i = 0; i < par.length; i++){
+                if(par[i].category_id == 1){
+                    count_1 += 1
                 }
+                if(par[i].category_id == 2){
+                    count_2 += 1
+                }
+                if(par[i].category_id == 3){
+                    count_3 += 1
+                }
+                if(par[i].category_id == 4){
+                    count_4 += 1
+                }
+                if(par[i].category_id == 5){
+                    count_5 += 1
+                }
+                if(par[i].category_id == 6){
+                    count_6 += 1
+                }
+                if(par[i].category_id == 7){
+                    count_7 += 1
+                }
+            }
+
+            Categories.findAll()
+            .then((category) => {
+                res.json(
+                [{ 
+                    id: category[0].id,
+                    name: category[0].name,
+                    count: count_1,
+                }, {
+                    id: category[1].id,
+                    name: category[1].name,
+                    count: count_2, 
+                }, {
+                    id: category[2].id,
+                    name: category[2].name,
+                    count: count_3,
+                }, {
+                    id: category[3].id,
+                    name: category[3].name,
+                    count: count_4,
+                }, {
+                    id: category[4].id,
+                    name: category[4].name,
+                    count: count_5,
+                }, {
+                    id: category[5].id,
+                    name: category[5].name,
+                    count: count_6,
+                }, {
+                    id: category[6].id,
+                    name: category[6].name,
+                    count: count_7
+                }])
             })
-            res.json({
-                count_3: 'testing...'
-            })
+            .then(err => console.log(err))
         })
         .catch(err => console.log(err))
 
